10d32262edb22389d88fd6d1568cf475457c1e55
Sergio Esteban Junco Castro <sergio.junco@disc-it.global>
2022-12-09 17:47:38 -0500

    ApiGateway

---
+++ APIGateway_Soluciones_Agiles/Extensions/NoEncoding_ExtensionMethod.cs
@@ -0,0 +1,11 @@
+﻿namespace APIGateway_Soluciones_Agiles.Extensions
+{
+    public class NoEncoding_ExtensionMethod: DelegatingHandler
+    {
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage re, CancellationToken cancellationToken)
+        {
+            re.Headers.AcceptEncoding.Clear();
+            return await base.SendAsync(re, cancellationToken);
+        }
+    }
+}
---
+++ APIGateway_Soluciones_Agiles/Program.cs
@@ -0,0 +1,34 @@
+using APIGateway_Soluciones_Agiles.Extensions;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+// Add services to the container.
+
+builder.Services.AddOcelot().AddDelegatingHandler<NoEncoding_ExtensionMethod>(true);
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseOcelot().Wait();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
